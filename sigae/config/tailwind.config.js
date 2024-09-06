0b93620a561eecd7739b2c0fd912ae876dd1f49d
mateo123456789 <mcva1523@gmail.com>
2024-09-06 12:53:21 -0300

    Implement Tailwindcss from scratch

---
+++ sigae/config/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '../app/views/*.html',    // Ruta a tus archivos HTML (si los tienes)
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1DA1F2',   // Un ejemplo de color personalizado
+        secondary: '#14171A',
+      },
+    },
+  },
+  plugins: [],
+}
