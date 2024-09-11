861220344e00f9d13c9ae72976363f8e4cf3887b
MoisesBenavides <moisesbp2706@gmail.com>
2024-09-11 14:43:10 -0300

    Add privilegies for cliente and ejecutivo roles

--- Segunda Entrega/BASE DE DATOS/sql_permisos/permisos_version0_11_1.sql
+++ Segunda Entrega/BASE DE DATOS/sql_permisos/permisos_version0_11_1.sql
@@ -1,10 +1,10 @@
 -- CREACION DE ROLES
 CREATE ROLE 'gerente';
 CREATE ROLE 'cliente';
+CREATE ROLE 'ejecutivo';
 CREATE ROLE 'cajero';
 CREATE ROLE 'jefe_diagnostico';
 CREATE ROLE 'jefe_taller';
-CREATE ROLE 'ejecutivo';
 CREATE ROLE 'valet_parking';
 
 -- PERMISOS
@@ -13,22 +13,25 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON aio.* TO 'gerente';
 
 -- cliente
 GRANT SELECT, INSERT, UPDATE, DELETE ON aio.cliente TO 'cliente';
-
-GRANT SELECT, INSERT, UPDATE, DELETE ON aio.vehiculo TO 'cliente';
 GRANT SELECT, INSERT, UPDATE, DELETE ON aio.tiene TO 'cliente';
+GRANT SELECT, INSERT, UPDATE ON aio.vehiculo TO 'cliente';
 
-GRANT SELECT, INSERT, UPDATE ON aio.servicio TO 'cliente';
-GRANT SELECT, INSERT, UPDATE ON aio.taller TO 'cliente';
-GRANT SELECT, INSERT, UPDATE ON aio.parking TO 'cliente';
-GRANT SELECT, INSERT, UPDATE ON aio.numero_plaza TO 'cliente';
+GRANT SELECT, INSERT ON aio.servicio TO 'cliente';
+GRANT SELECT, INSERT ON aio.taller TO 'cliente';
+GRANT SELECT, INSERT ON aio.parking TO 'cliente';
+GRANT SELECT, INSERT ON aio.numero_plaza TO 'cliente';
 
 GRANT SELECT, INSERT, UPDATE ON aio.orden TO 'cliente';
-GRANT SELECT, INSERT, UPDATE ON aio.detalle_orden_producto TO 'cliente';
-GRANT SELECT, INSERT, UPDATE ON aio.detalle_orden_servicio TO 'cliente';
+GRANT SELECT, INSERT ON aio.detalle_orden_producto TO 'cliente';
+GRANT SELECT, INSERT ON aio.detalle_orden_servicio TO 'cliente';
 
 GRANT SELECT ON aio.producto TO 'cliente';
 GRANT SELECT ON aio.neumatico TO 'cliente';
 GRANT SELECT ON aio.otro_producto TO 'cliente';
+
+-- ejecutivo
+/*El ejecutivo puede ingresar la marca, modelo y color de un vehículo*/
+GRANT SELECT, UPDATE ON aio.vehiculo TO 'ejecutivo';
 
 -- SET ROLES
 SET ROLE 'gerente';
