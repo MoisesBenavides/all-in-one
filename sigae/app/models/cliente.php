e7692c0565a2ebe5548a64c92c522264671e2afc
MoisesBenavides <moisesbp2706@gmail.com>
2024-09-13 12:42:37 -0300

    Add getters and setters

--- sigae/app/models/cliente.php
+++ sigae/app/models/cliente.php
@@ -8,4 +8,57 @@ class Cliente{
     private $apellido;
     private $telefono;
 
+    public function getId(){
+        return $this->id;
+    }
+
+    public function setId($id){
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getCi(){
+        return $this->ci;
+    }
+
+    public function setCi($ci){
+        $this->ci = $ci;
+        return $this;
+    }
+
+    public function getEmail(){
+        return $this->email;
+    }
+
+    public function setEmail($email){
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getNombre(){
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre){
+        $this->nombre = $nombre;
+        return $this;
+    }
+
+    public function getApellido(){
+        return $this->apellido;
+    }
+
+    public function setApellido($apellido){
+        $this->apellido = $apellido;
+        return $this;
+    }
+ 
+    public function getTelefono(){
+        return $this->telefono;
+    }
+
+    public function setTelefono($telefono){
+        $this->telefono = $telefono;
+        return $this;
+    }
 }
