a6835e62d5dcc785115d3a720ebbc38a5afa54a9
MoisesBenavides <moisesbp2706@gmail.com>
2024-09-17 20:30:50 -0300

    Add admin user to sql scripts

--- Segunda_Entrega/BASE_DE_DATOS/sql_permisos/usuarios_db.sql
+++ Segunda_Entrega/BASE_DE_DATOS/sql_permisos/usuarios_db.sql
@@ -1,7 +1,51 @@
 -- CREACION DE USUARIOS
--- cliente
-CREATE USER 'def_client'@'localhost' IDENTIFIED BY '';
+-- admin
+CREATE USER 'admin'@'localhost' IDENTIFIED BY '1234';
+
+-- cliente predeterminado
+CREATE USER 'def_cliente'@'localhost' IDENTIFIED BY '';
+
+-- gerente predeterminado
+CREATE USER 'def_gerente'@'localhost' IDENTIFIED BY '';
+
+-- ejecutivo predeterminado
+CREATE USER 'def_ejecutivo'@'localhost' IDENTIFIED BY '';
+
+-- cajero predeterminado
+CREATE USER 'def_cajero'@'localhost' IDENTIFIED BY '';
+
+-- jefe de diagnostico predeterminado
+CREATE USER 'def_jefe_diagnostico'@'localhost' IDENTIFIED BY '';
+
+-- jefe de taller predeterminado
+CREATE USER 'def_jefe_taller'@'localhost' IDENTIFIED BY '';
+
+-- valet parking predeterminado
+CREATE USER 'def_valet_parking'@'localhost' IDENTIFIED BY '';
+
+-------------------------------------------------------------------
 
 -- ASIGNAR ROLES A USUARIOS
--- cliente
-GRANT 'cliente' TO 'def_client'@'localhost';
+-- admin
+GRANT 'admin_rol' TO 'admin'@'localhost';
+
+-- cliente predeterminado
+GRANT 'cliente' TO 'def_cliente'@'localhost';
+
+-- gerente predeterminado
+GRANT 'gerente' TO 'def_gerente'@'localhost';
+
+-- ejecutivo predeterminado
+GRANT 'ejecutivo' TO 'def_ejecutivo'@'localhost';
+
+-- cajero predeterminado
+GRANT 'cajero' TO 'def_cajero'@'localhost';
+
+-- jefe de diagnostico predeterminado
+GRANT 'jefe_diagnostico' TO 'def_jefe_diagnostico'@'localhost';
+
+-- jefe de taller predeterminado
+GRANT 'jefe_taller' TO 'def_jefe_taller'@'localhost';
+
+-- valet parking predeterminado
+GRANT 'valet_parking' TO 'def_valet_parking'@'localhost';
