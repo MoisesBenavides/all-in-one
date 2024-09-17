d3e87df1a50d715e8e6eb94545d46457d9961cab
MoisesBenavides <moisesbp2706@gmail.com>
2024-09-17 19:12:18 -0300

    Moddify restrictions in table columns

--- Segunda_Entrega/BASE_DE_DATOS/sql_ddl/ddl_v0.12.sql
+++ Segunda_Entrega/BASE_DE_DATOS/sql_ddl/ddl_v0.12.sql
@@ -2,11 +2,12 @@
 -- Cliente
 CREATE TABLE cliente (
     id INT NOT NULL,
-    ci INT NOT NULL UNIQUE,
+    ci INT NULL UNIQUE,
     email VARCHAR(63) NOT NULL UNIQUE,
+    hash_contrasena VARCHAR(255) NULL UNIQUE,
     nombre VARCHAR(23) NOT NULL,
-    apellido VARCHAR(23) NOT NULL,
-    telefono INT NOT NULL,
+    apellido VARCHAR(23) NULL,
+    telefono INT NULL,
     PRIMARY KEY (id)
 );
 
