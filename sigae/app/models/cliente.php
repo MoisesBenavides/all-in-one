d526f5d4d23af42a2fb5046c3513b6ee02b64914
MoisesBenavides <moisesbp2706@gmail.com>
2024-09-19 18:46:13 -0300

    Implement add new Cliente

--- sigae/app/models/cliente.php
+++ sigae/app/models/cliente.php
@@ -73,4 +73,53 @@ class Cliente{
         return $this;
     }
 
+    public function agregarCliente($email, $contrasena, $nombre, $apellido){
+        $hash_contrasena = password_hash($contrasena, PASSWORD_BCRYPT);
+        try{
+            $conn = conectarDB("def_cliente", "", "localhost");
+
+            if($this->nuevoEmail($email)){
+                // Consulta con parámetros
+                $stmt = $conn->prepare('INSERT INTO cliente (email, hash_contrasena, nombre, apellido) VALUES (:email, :hash_con, :nom, :ape)');
+            
+                $stmt->bindParam(':email', $email);
+                $stmt->bindParam(':hash_con', $hash_contrasena);
+                $stmt->bindParam(':nom', $nombre);
+                $stmt->bindParam(':ape', $apellido);
+                
+                $stmt->execute();
+            } else{
+                echo "Ya existe un usuario con el correo ingresado.";
+            }
+
+        } catch (Exception $e){
+            echo $e->getMessage();
+
+        } finally {
+            $conn = null;
+        }
+    }
+
+    function nuevoEmail($email){
+        try{
+            $conn = conectarDB("def_cliente", "", "localhost");
+
+            $stmt = $conn->prepare('SELECT COUNT(*) FROM cliente WHERE email = :email');
+        
+            $stmt->bindParam(':email', $email);
+            
+            $stmt->execute();
+    
+            $count = $stmt->fetchColumn();
+
+            return $count == 0 ? true : false;
+
+        } catch (Exception $e){
+            echo $e->getMessage();
+
+        } finally {
+            $conn = null;
+        }
+    }
+
 }
