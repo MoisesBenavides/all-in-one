abacef65876d75d9eadd1596bcb6b6c0d703a3dc
MoisesBenavides <moisesbp2706@gmail.com>
2024-09-19 13:34:44 -0300

    Implement functions for client account functions

--- sigae/app/controllers/controladorCliente.php
+++ sigae/app/controllers/controladorCliente.php
@@ -8,8 +8,31 @@ class ControladorCliente{
     public function __construct(){
         $this->cliente=new Cliente();
     }
-
-    function mostrarHome(){
+    function login(){
+        include '../app/views/account/login.html';
+    }
+    function doLogin(){
+        //implementar validacion
+        //enviar formulario a modelo Cliente
+        //redireccionar a home
+        include '../app/views/client/homeCliente.html';
+    }
+    function signup(){
+        include '../app/views/account/signup.html';
+    }
+    function doSignup(){
+        //implementar validacion
+        //enviar formulario a modelo Cliente
+        //redireccionar a home
+        include '../app/views/client/homeCliente.html';
+    }
+    function forgotPassword(){
+        //implementar validacion
+        //enviar formulario a modelo Cliente
+        //redireccionar a login
+        include '../app/views/client/login.html';
+    }
+    function home(){
         include '../app/views/client/homeCliente.html';
     }
 }
