326d1dac35e4b0d0a780f850e7922622c53759f8
MoisesBenavides <moisesbp2706@gmail.com>
2024-09-22 18:28:26 -0300

    Add enum for tipo de vehiculo

---
+++ sigae/app/models/TipoVehiculo.php
@@ -0,0 +1,9 @@
+<?php
+
+enum TipoVehiculo: String{
+    case Moto = 'moto';
+    case Auto = 'auto';
+    case Camioneta = 'camioneta';
+    case Camion = 'camion';
+    case Utilitario = 'utilitario';
+}
