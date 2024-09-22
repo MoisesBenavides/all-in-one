b1da442d28ede7a24ee48984a7068279aa64247e
MoisesBenavides <moisesbp2706@gmail.com>
2024-09-22 02:03:18 -0300

    Add Taller model class

--- sigae/app/models/Taller.php
+++ sigae/app/models/Taller.php
@@ -1,5 +1,16 @@
 <?php
 
-class Taller{
-    
+class Taller extends Servicio{
+    private $tipo;
+    private $descripcion;
+    private $diagnostico;
+    private $tiempo_estimado;
+
+    public function __construct($tipo, $descripcion, $diagnostico, $tiempo_estimado, $id, $precio, $fecha_inicio, $fecha_final, EstadoServicio $estado){
+        parent::__construct($id, $precio, $fecha_inicio, $fecha_final, $estado);
+        $this->tipo = $tipo;
+        $this->descripcion = $descripcion;
+        $this->diagnostico = $diagnostico;
+        $this->tiempo_estimado = $tiempo_estimado;
+    }
 }
