d683fb1286fd04fd49f73910de1d877d00dd94e5
MoisesBenavides <moisesbp2706@gmail.com>
2024-09-23 02:28:39 -0300

    Add register now functions for service reservation

--- sigae/app/models/Vehiculo.php
+++ sigae/app/models/Vehiculo.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once '../app/db_config/db_conn.php';
+
 class Vehiculo
 {
     private $matricula;
@@ -8,8 +10,7 @@ class Vehiculo
     private $tipo;
     private $color;
 
-    public function __construct($matricula, $marca, $modelo, $tipo, $color)
-    {
+    public function __construct($matricula, $marca, $modelo, $tipo, $color){
         $this->matricula = $matricula;
         $this->marca = $marca;
         $this->modelo = $modelo;
@@ -66,4 +67,54 @@ class Vehiculo
     {
         $this->color = $color;
     }
+
+    public function registrarYa(){
+        $matricula = $this->getMatricula();
+        $tipo = $this->getTipo();
+        try {
+            $conn = conectarDB("def_cliente", "password_cliente", "localhost");
+            
+            if ($conn === false) {
+                throw new Exception("No se pudo conectar a la base de datos.");
+            }
+    
+            $stmt = $conn->prepare('INSERT INTO vehiculo (matricula, tipo) VALUES (:mat, :tipo)');
+            
+            $stmt->bindParam(':mat', $matricula);
+            $stmt->bindParam(':tipo', $tipo);
+                
+            $stmt->execute();
+            return true; // Registrar;
+
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+            error_log("Error al registrar el vehículo: " . $e->getMessage());
+            return false;
+            
+        } finally {
+            $conn = null;
+        }
+        
+
+    }
+
+    public static function existeMatricula($matricula) {
+        try {
+            $conn = conectarDB("def_cliente", "password_cliente", "localhost");
+            $stmt = $conn->prepare('SELECT COUNT(*) FROM vehiculo WHERE matricula = :mat');
+            $stmt->bindParam(':mat', $matricula);
+            $stmt->execute();
+            
+            $count = $stmt->fetchColumn();
+
+            return $count != 0;
+
+        } catch (Exception $e) {
+            error_log($e->getMessage()); // Registro del error en el log
+            return false; // Devuelve false si hubo un error de base de datos
+        } finally {
+            $conn = null;
+        }
+    }
+    
 }
