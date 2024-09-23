505fd2360609c021e89d3ccfe33373695bb6df54
MoisesBenavides <moisesbp2706@gmail.com>
2024-09-22 21:20:53 -0300

    Add constructor, attributes, getters and setters

--- sigae/app/models/Vehiculo.php
+++ sigae/app/models/Vehiculo.php
@@ -1,5 +1,69 @@
 <?php
 
-class Vehiculo{
-    
+class Vehiculo
+{
+    private $matricula;
+    private $marca;
+    private $modelo;
+    private $tipo;
+    private $color;
+
+    public function __construct($matricula, $marca, $modelo, $tipo, $color)
+    {
+        $this->matricula = $matricula;
+        $this->marca = $marca;
+        $this->modelo = $modelo;
+        $this->tipo = $tipo;
+        $this->color = $color;
+    }
+
+    public function getMatricula()
+    {
+        return $this->matricula;
+    }
+
+    public function setMatricula($matricula)
+    {
+        $this->matricula = $matricula;
+    }
+
+    public function getMarca()
+    {
+        return $this->marca;
+    }
+
+    public function setMarca($marca)
+    {
+        $this->marca = $marca;
+    }
+
+    public function getModelo()
+    {
+        return $this->modelo;
+    }
+
+    public function setModelo($modelo)
+    {
+        $this->modelo = $modelo;
+    }
+
+    public function getTipo()
+    {
+        return $this->tipo;
+    }
+
+    public function setTipo($tipo)
+    {
+        $this->tipo = $tipo;
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    public function setColor($color)
+    {
+        $this->color = $color;
+    }
 }
