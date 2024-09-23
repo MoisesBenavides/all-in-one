7849f37ffd46df4d37e9edcda67a6d14822424a7
MoisesBenavides <moisesbp2706@gmail.com>
2024-09-23 06:51:54 -0300

    Add model class Parking

--- sigae/app/models/Parking.php
+++ sigae/app/models/Parking.php
@@ -1,5 +1,35 @@
 <?php
 
-class Parking{
-    
+require_once '../app/db_config/db_conn.php';
+require_once 'TipoPlazaParking.php';
+require_once 'Servicio.php';
+
+class Parking extends Servicio{
+    private $largo_plazo;
+    private TipoPlazaParking $tipo_plaza;
+
+    public function __construct($largo_plazo, $tipo_plaza, $id, $precio, $fecha_inicio, $fecha_final){
+        parent::__construct($id, $precio, $fecha_inicio, $fecha_final);
+        $this->largo_plazo = $largo_plazo;
+        $this->tipo_plaza = $tipo_plaza;
+    }
+
+    public function getLargo_plazo(){
+        return $this->largo_plazo;
+    }
+
+    public function setLargo_plazo($largo_plazo){
+        $this->largo_plazo = $largo_plazo;
+
+        return $this;
+    }
+
+    public function cambiarServicio(){
+    }
+
+    public function cancelarServicio(){
+    }
+
+    public function getServicios(){
+    }
 }
