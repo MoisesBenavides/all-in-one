7b600972f1c598db13f42b6d1162c9ae9c545e2f
mateo123456789 <mcva1523@gmail.com>
2024-10-01 23:21:32 -0300

    made albisoft folder with tailwind and npm config

---
+++ diseno/Albisoft/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.html',
+    './src/**/*.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
