8f54680f055c92707980588a098f6b700e1b87e3
MoisesBenavides <moisesbp2706@gmail.com>
2024-10-11 03:49:53 -0300

    Fix namespace for kernel

--- sigae/public/index.php
+++ sigae/public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Kernel;
+use Sigae\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
--- sigae/src/Kernel.php
+++ sigae/src/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace Sigae;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
