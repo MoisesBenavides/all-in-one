869abd6de817dad6aea77df94e511869a9a137a9
MoisesBenavides <moisesbp2706@gmail.com>
2024-10-20 14:40:21 -0300

    Add products controlller classes

--- sigae/src/controllers/ControladorNeumatico.php
+++ sigae/src/controllers/ControladorNeumatico.php
@@ -2,3 +2,10 @@
 
 namespace Sigae\Controllers;
 use Sigae\Models\Neumatico;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class ControladorNeumatico extends AbstractController{
+
+}
--- sigae/src/controllers/ControladorOtroProducto.php
+++ sigae/src/controllers/ControladorOtroProducto.php
@@ -2,3 +2,10 @@
 
 namespace Sigae\Controllers;
 use Sigae\Models\OtroProducto;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class ControladorOtroProducto extends AbstractController{
+
+}
