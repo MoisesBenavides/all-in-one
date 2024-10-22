36de85be5c239325dc21b1ecf74fc5b5076b9d84
mateo123456789 <mcva1523@gmail.com>
2024-10-22 01:15:57 -0300

    correct little typo on line 135

--- sigae/src/controllers/ControladorParking.php
+++ sigae/src/controllers/ControladorParking.php
@@ -132,7 +132,7 @@ class ControladorParking extends AbstractController{
                 array_keys($_POST)
             );
         }
-        return $this->render('client/reservaraParkingSimple.html.twig', [
+        return $this->render('client/reservarParkingSimple.html.twig', [
             'response' => $response  // Aquí pasa la respuesta a la vista
         ]);
     }
