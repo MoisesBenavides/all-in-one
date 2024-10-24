49c94ef35f77e6f997f98c22fe83f8ba2c6f0f0c
MoisesBenavides <moisesbp2706@gmail.com>
2024-10-24 12:24:25 -0300

    Add debug for parking controller

--- sigae/src/controllers/ControladorParking.php
+++ sigae/src/controllers/ControladorParking.php
@@ -277,6 +277,7 @@ class ControladorParking extends AbstractController{
             $this->parking = new Parking($largo_plazo, $tipo_plaza, null, $precio, $fecha_inicio, $fecha_final);
 
             $this->parking->comenzarTransaccion();
+            error_log(print_r($this->parking->conn, true));
 
             if(!$this->parking->reservarServicio($matricula)){
                 $this->parking->deshacerTransaccion();
