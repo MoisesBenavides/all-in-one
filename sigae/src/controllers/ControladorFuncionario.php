9eb7cd4cef6db170b8bc406ad18ecfb99a9d9119
MoisesBenavides <moisesbp2706@gmail.com>
2024-11-02 13:13:56 -0300

    Add user management function

--- sigae/src/controllers/ControladorFuncionario.php
+++ sigae/src/controllers/ControladorFuncionario.php
@@ -112,6 +112,15 @@ class ControladorFuncionario extends AbstractController {
         }
     }
 
+    function userManagement(): Response{
+        $rol=$_SESSION['rol'];
+        if ($rol=='gerente'){
+            return $this->render('employee/manager/listaUsuarios.html.twig');
+        } else{
+            return $this->render('errors/errorAcceso.html.twig');
+        }
+    }
+
     private function validarUsuario($str, $max) {
         /* Verifica si el nombre de usuario $str cumple con ciertos criterios como:
             - El primer carácter debe ser una letra o un número.
