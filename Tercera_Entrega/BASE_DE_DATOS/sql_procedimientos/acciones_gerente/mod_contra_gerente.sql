67abd7c7c2b7471d5fd8a144a77f8db92d775c07
MoisesBenavides <moisesbp2706@gmail.com>
2024-11-03 20:29:21 -0300

    Add procedures for password modification

---
+++ Tercera_Entrega/BASE_DE_DATOS/sql_procedimientos/acciones_gerente/mod_contra_gerente.sql
@@ -0,0 +1,43 @@
+DELIMITER $$
+
+CREATE PROCEDURE mod_contra_gerente(
+    IN nombre_usuario VARCHAR(50),
+    IN nombre_host VARCHAR(50),
+    IN nueva_contrasena VARCHAR(60)
+)
+BEGIN
+    -- Obtiene el rol del usuario que ejecuta el procedimento
+    DECLARE rol_actual VARCHAR(50);
+    SELECT CURRENT_ROLE() INTO rol_actual;
+
+    -- Vefirica si el rol actual es autorizado
+    IF rol_actual = 'admin_rol' THEN
+
+        DECLARE EXIT HANDLER FOR SQLEXCEPTION
+        BEGIN
+            -- En caso de error, anula la transaccion y envia un mensaje de error
+            ROLLBACK;
+            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Error al cambiar la clave';
+        END;
+
+        -- Inicia una transaccion
+        START TRANSACTION;
+
+        -- Cambia la contraseña del usuario gerente
+        SET @query = CONCAT('ALTER USER ''', nombre_usuario, '''@''', nombre_host, ''' IDENTIFIED BY ''', nueva_contrasena, '''');
+        PREPARE stmt FROM @query;
+        EXECUTE stmt;
+        DEALLOCATE PREPARE stmt;
+
+        -- Confirma la transacción
+        COMMIT;
+
+        -- Mensaje de modificacion de contraseña exitosa
+        SELECT CONCAT('Cambio de clave al usuario ', nombre_actual, '@', host_actual, ' realizado exitosamente.') AS resultado;
+    ELSE
+        -- Si el rol actual no es autorizado, muestra un mensaje de error
+        SELECT 'No tiene permisos para cambiar la clave de un usuario "gerente".' AS resultado;
+    END IF;
+END $$
+
+DELIMITER ;
