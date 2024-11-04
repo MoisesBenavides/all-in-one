c7cac4af6f4e9f1f959866a9bd1ecfcef4c6fec4
MoisesBenavides <moisesbp2706@gmail.com>
2024-11-03 23:54:19 -0300

    Remove role validation in procedures

--- Tercera_Entrega/BASE_DE_DATOS/sql_procedimientos/acciones_cajero/mod_contra_cajero.sql
+++ Tercera_Entrega/BASE_DE_DATOS/sql_procedimientos/acciones_cajero/mod_contra_cajero.sql
@@ -6,38 +6,27 @@ CREATE PROCEDURE mod_contra_cajero(
     IN nueva_contrasena VARCHAR(60)
 )
 BEGIN
-    -- Obtiene el rol del usuario que ejecuta el procedimento
-    DECLARE rol_actual VARCHAR(50);
-    SELECT CURRENT_ROLE() INTO rol_actual;
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        -- En caso de error, anula la transaccion y envia un mensaje de error
+        ROLLBACK;
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Error al cambiar la clave';
+    END;
 
-    -- Vefirica si el rol actual es autorizado
-    IF rol_actual = 'gerente' OR 'admin_rol' THEN
+    -- Inicia una transaccion
+    START TRANSACTION;
 
-        DECLARE EXIT HANDLER FOR SQLEXCEPTION
-        BEGIN
-            -- En caso de error, anula la transaccion y envia un mensaje de error
-            ROLLBACK;
-            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Error al cambiar la clave';
-        END;
+    -- Cambia la contraseña del usuario cajero
+    SET @query = CONCAT('ALTER USER ''', nombre_usuario, '''@''', nombre_host, ''' IDENTIFIED BY ''', nueva_contrasena, '''');
+    PREPARE stmt FROM @query;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
 
-        -- Inicia una transaccion
-        START TRANSACTION;
+    -- Confirma la transacción
+    COMMIT;
 
-        -- Cambia la contraseña del usuario cajero
-        SET @query = CONCAT('ALTER USER ''', nombre_usuario, '''@''', nombre_host, ''' IDENTIFIED BY ''', nueva_contrasena, '''');
-        PREPARE stmt FROM @query;
-        EXECUTE stmt;
-        DEALLOCATE PREPARE stmt;
-
-        -- Confirma la transacción
-        COMMIT;
-
-        -- Mensaje de modificacion de contraseña exitosa
-        SELECT CONCAT('Cambio de clave al usuario ', nombre_actual, '@', host_actual, ' realizado exitosamente.') AS resultado;
-    ELSE
-        -- Si el rol actual no es autorizado, muestra un mensaje de error
-        SELECT 'No tiene permisos para cambiar la clave de un usuario "cajero".' AS resultado;
-    END IF;
+    -- Mensaje de modificacion de contraseña exitosa
+    SELECT CONCAT('Cambio de clave al usuario ', nombre_actual, '@', host_actual, ' realizado exitosamente.') AS resultado;
 END $$
 
 DELIMITER ;
--- Tercera_Entrega/BASE_DE_DATOS/sql_procedimientos/acciones_ejecutivo/alta_ejecutivo.sql
+++ Tercera_Entrega/BASE_DE_DATOS/sql_procedimientos/acciones_ejecutivo/alta_ejecutivo.sql
@@ -6,27 +6,15 @@ CREATE PROCEDURE alta_ejecutivo(
     IN nueva_contrasena VARCHAR(60)
 )
 BEGIN
-    -- Obtiene el rol del usuario que ejecuta el procedimento
-    DECLARE rol_actual VARCHAR(50);
-    SELECT CURRENT_ROLE() INTO rol_actual;
+    -- Construye la consulta de creacion de usuario con rol de ejecutivo
+    SET @query = CONCAT('CREATE USER "', nombre_usuario, '"@"', nombre_host, '" IDENTIFIED BY "', nueva_contrasena, '" DEFAULT ROLE "ejecutivo";');
 
-    -- Vefirica si el rol actual es autorizado
-    IF rol_actual = 'jefe_taller' OR 'admin_rol' THEN
-        -- Construye la consulta de creacion de usuario con rol de ejecutivo
-        SET @query = CONCAT('CREATE USER "', nombre_usuario, '"@"', nombre_host, '" IDENTIFIED BY "', nueva_contrasena, '" DEFAULT ROLE "ejecutivo";');
+    PREPARE stmt FROM @query;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
 
-        PREPARE stmt FROM @query;
-
-        EXECUTE stmt;
-
-        DEALLOCATE PREPARE stmt;
-
-        -- Mensaje de alta exitosa
-        SELECT CONCAT('Usuario ', nombre_usuario, '@', nombre_host, ' creado exitosamente.') AS resultado;
-    ELSE
-        -- Si el rol actual no es autorizado, muestra un mensaje de error
-        SELECT 'No tiene permisos para dar de alta un usuario "ejecutivo".' AS resultado;
-    END IF;
+    -- Mensaje de alta exitosa
+    SELECT CONCAT('Usuario ', nombre_usuario, '@', nombre_host, ' creado exitosamente.') AS resultado;
 END $$
 
 DELIMITER ;
