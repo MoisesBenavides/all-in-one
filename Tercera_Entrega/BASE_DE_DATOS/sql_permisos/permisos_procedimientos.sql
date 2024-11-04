6efa1fe92998287f57b7d58811cdfa17db19cc3e
MoisesBenavides <moisesbp2706@gmail.com>
2024-11-04 06:52:54 -0300

    Add permissions for procedure to change user password

--- Tercera_Entrega/BASE_DE_DATOS/sql_permisos/permisos_procedimientos.sql
+++ Tercera_Entrega/BASE_DE_DATOS/sql_permisos/permisos_procedimientos.sql
@@ -2,45 +2,56 @@
 GRANT EXECUTE ON PROCEDURE aio_db.alta_gerente TO 'admin_rol';
 GRANT EXECUTE ON PROCEDURE aio_db.modificacion_gerente TO 'admin_rol';
 GRANT EXECUTE ON PROCEDURE aio_db.baja_gerente TO 'admin_rol';
+GRANT EXECUTE ON PROCEDURE aio_db.mod_contra_gerente TO 'admin_rol';
 
 GRANT EXECUTE ON PROCEDURE aio_db.alta_jefe_diagnostico TO 'admin_rol';
 GRANT EXECUTE ON PROCEDURE aio_db.modificacion_jefe_diagnostico TO 'admin_rol';
 GRANT EXECUTE ON PROCEDURE aio_db.baja_jefe_diagnostico TO 'admin_rol';
+GRANT EXECUTE ON PROCEDURE aio_db.mod_contra_jefe_diagnostico TO 'admin_rol';
 
 GRANT EXECUTE ON PROCEDURE aio_db.alta_jefe_taller TO 'admin_rol';
 GRANT EXECUTE ON PROCEDURE aio_db.modificacion_jefe_taller TO 'admin_rol';
 GRANT EXECUTE ON PROCEDURE aio_db.baja_jefe_taller TO 'admin_rol';
+GRANT EXECUTE ON PROCEDURE aio_db.mod_contra_jefe_taller TO 'admin_rol';
 
 GRANT EXECUTE ON PROCEDURE aio_db.alta_cajero TO 'admin_rol';
 GRANT EXECUTE ON PROCEDURE aio_db.modificacion_cajero TO 'admin_rol';
 GRANT EXECUTE ON PROCEDURE aio_db.baja_cajero TO 'admin_rol';
+GRANT EXECUTE ON PROCEDURE aio_db.mod_contra_cajero TO 'admin_rol';
 
 GRANT EXECUTE ON PROCEDURE aio_db.alta_ejecutivo TO 'admin_rol';
 GRANT EXECUTE ON PROCEDURE aio_db.modificacion_ejecutivo TO 'admin_rol';
 GRANT EXECUTE ON PROCEDURE aio_db.baja_ejecutivo TO 'admin_rol';
+GRANT EXECUTE ON PROCEDURE aio_db.mod_contra_ejecutivo TO 'admin_rol';
 
 GRANT EXECUTE ON PROCEDURE aio_db.alta_valet_parking TO 'admin_rol';
 GRANT EXECUTE ON PROCEDURE aio_db.modificacion_valet_parking TO 'admin_rol';
 GRANT EXECUTE ON PROCEDURE aio_db.baja_valet_parking TO 'admin_rol';
+GRANT EXECUTE ON PROCEDURE aio_db.mod_contra_valet_parking TO 'admin_rol';
 
 -- Gerente
 GRANT EXECUTE ON PROCEDURE aio_db.alta_jefe_diagnostico TO 'gerente';
 GRANT EXECUTE ON PROCEDURE aio_db.modificacion_jefe_diagnostico TO 'gerente';
 GRANT EXECUTE ON PROCEDURE aio_db.baja_jefe_diagnostico TO 'gerente';
+GRANT EXECUTE ON PROCEDURE aio_db.mod_contra_jefe_diagnostico TO 'gerente';
 
 GRANT EXECUTE ON PROCEDURE aio_db.alta_jefe_taller TO 'gerente';
 GRANT EXECUTE ON PROCEDURE aio_db.modificacion_jefe_taller TO 'gerente';
 GRANT EXECUTE ON PROCEDURE aio_db.baja_jefe_taller TO 'gerente';
+GRANT EXECUTE ON PROCEDURE aio_db.mod_contra_jefe_taller TO 'gerente';
 
 GRANT EXECUTE ON PROCEDURE aio_db.alta_cajero TO 'gerente';
 GRANT EXECUTE ON PROCEDURE aio_db.modificacion_cajero TO 'gerente';
 GRANT EXECUTE ON PROCEDURE aio_db.baja_cajero TO 'gerente';
+GRANT EXECUTE ON PROCEDURE aio_db.mod_contra_cajero TO 'gerente';
 
 GRANT EXECUTE ON PROCEDURE aio_db.alta_valet_parking TO 'gerente';
 GRANT EXECUTE ON PROCEDURE aio_db.modificacion_valet_parking TO 'gerente';
 GRANT EXECUTE ON PROCEDURE aio_db.baja_valet_parking TO 'gerente';
+GRANT EXECUTE ON PROCEDURE aio_db.mod_contra_valet_parking TO 'gerente';
 
 -- Jefe de Taller
 GRANT EXECUTE ON PROCEDURE aio_db.alta_ejecutivo TO 'jefe_taller';
 GRANT EXECUTE ON PROCEDURE aio_db.modificacion_ejecutivo TO 'jefe_taller';
 GRANT EXECUTE ON PROCEDURE aio_db.baja_ejecutivo TO 'jefe_taller';
+GRANT EXECUTE ON PROCEDURE aio_db.mod_contra_ejecutivo TO 'jefe_taller';
