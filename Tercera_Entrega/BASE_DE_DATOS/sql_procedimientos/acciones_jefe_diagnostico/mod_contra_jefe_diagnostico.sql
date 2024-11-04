896aa6972868dda3cc8196f9f9ebdbc942c956ca
MoisesBenavides <moisesbp2706@gmail.com>
2024-11-03 23:13:35 -0300

    Update queries for procedures

--- Tercera_Entrega/BASE_DE_DATOS/sql_procedimientos/acciones_jefe_diagnostico/mod_contra_jefe_diagnostico.sql
+++ Tercera_Entrega/BASE_DE_DATOS/sql_procedimientos/acciones_jefe_diagnostico/mod_contra_jefe_diagnostico.sql
@@ -6,14 +6,7 @@ CREATE PROCEDURE mod_contra_jefe_diagnostico(
     IN nueva_contrasena VARCHAR(60)
 )
 BEGIN
-    -- Obtiene el rol del usuario que ejecuta el procedimento
-    DECLARE rol_actual VARCHAR(50);
-    SELECT CURRENT_ROLE() INTO rol_actual;
-
-    -- Vefirica si el rol actual es autorizado
-    IF rol_actual = 'gerente' OR 'admin_rol' THEN
-
-        DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
         BEGIN
             -- En caso de error, anula la transaccion y envia un mensaje de error
             ROLLBACK;
@@ -34,10 +27,6 @@ BEGIN
 
         -- Mensaje de modificacion de contraseña exitosa
         SELECT CONCAT('Cambio de clave al usuario ', nombre_actual, '@', host_actual, ' realizado exitosamente.') AS resultado;
-    ELSE
-        -- Si el rol actual no es autorizado, muestra un mensaje de error
-        SELECT 'No tiene permisos para cambiar la clave de un usuario "jefe_diagnostico".' AS resultado;
-    END IF;
 END $$
 
 DELIMITER ;
--- Tercera_Entrega/BASE_DE_DATOS/sql_procedimientos/acciones_jefe_diagnostico/modificar_jefe_diagnostico.sql
+++ Tercera_Entrega/BASE_DE_DATOS/sql_procedimientos/acciones_jefe_diagnostico/modificar_jefe_diagnostico.sql
@@ -7,27 +7,15 @@ CREATE PROCEDURE modificar_jefe_diagnostico(
     IN nuevo_host VARCHAR(50)
 )
 BEGIN
-    -- Obtiene el rol del usuario que ejecuta el procedimento
-    DECLARE rol_actual VARCHAR(50);
-    SELECT CURRENT_ROLE() INTO rol_actual;
+    -- Construye la consulta de modificacion de usuario
+    SET @query = CONCAT('RENAME USER "', nombre_actual, '"@"', host_actual, '" TO "', nuevo_nombre, '"@"', nuevo_host, '";');
 
-    -- Vefirica si el rol actual es autorizado
-    IF rol_actual = 'gerente' OR 'admin_rol' THEN
-        -- Construye la consulta de modificacion de usuario
-        SET @query = CONCAT('RENAME USER "', nombre_actual, '"@"', host_actual, '" TO "', nuevo_nombre, '"@"', nuevo_host, '";');
+    PREPARE stmt FROM @query;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
 
-        PREPARE stmt FROM @query;
-
-        EXECUTE stmt;
-
-        DEALLOCATE PREPARE stmt;
-
-        -- Mensaje de modificacion exitosa
-        SELECT CONCAT('Usuario ', nombre_actual, '@', host_actual, ' modificado a ', nuevo_nombre, '@', nuevo_host, ' exitosamente.') AS resultado;
-    ELSE
-        -- Si el rol actual no es autorizado, muestra un mensaje de error
-        SELECT 'No tiene permisos para modificar un usuario "jefe_diagnostico".' AS resultado;
-    END IF;
+    -- Mensaje de modificacion exitosa
+    SELECT CONCAT('Usuario ', nombre_actual, '@', host_actual, ' modificado a ', nuevo_nombre, '@', nuevo_host, ' exitosamente.') AS resultado;
 END $$
 
 DELIMITER ;
