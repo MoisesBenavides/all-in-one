9e042a1db4da39b26aacee8495f581ba18b509cd
Moisés Benavides <moisesbp2706@gmail.com>
2024-11-07 23:32:27 -0300

    Add for cycle for validation of ids in order

--- sigae/src/controllers/ControladorOrden.php
+++ sigae/src/controllers/ControladorOrden.php
@@ -2,9 +2,12 @@
 
 namespace Sigae\Controllers;
 use Sigae\Models\Orden;
+use Sigae\Models\Producto;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
+use DateTimeZone;
+use DateTime;
 use Exception;
 
 class ControladorOrden extends AbstractController{
@@ -15,20 +18,42 @@ class ControladorOrden extends AbstractController{
 
         $rol=$_SESSION['rol'];
 
-        $productos = [];
-        $services = [];
+        $productosId = [];
+        $serviciosId = [];
 
         switch($rol){
             case 'cajero':
                 if(isset($_POST["product_ids"]) || isset($_POST["reservation_ids"]) && 
                 !empty($_POST["product_ids"]) || !empty($_POST["reservation_ids"])){
 
-                    $productos = $_POST["product_ids"];
-                    $servicios = $_POST["reservation_ids"];
+                    if(isset($_POST["id_cliente"]) && !empty($_POST["id_cliente"])){
+                        $productosId = $_POST["product_ids"];
+                        $serviciosId = $_POST["reservation_ids"];
+                        $idCliente = $_POST["id_cliente"];
+                        $fecha = $this->obtenerFechaHoraActual();
 
-                    if(!$this->validarListaIds($productos) || !$this->validarListaIds($servicios)){
-                       $response['errors'][] = "Lista de IDs contiene datos inválidos";
-                    }
+                        if(!$this->validarListaIds($productosId) || !$this->validarListaIds($serviciosId)){
+                        $response['errors'][] = "Lista de IDs contiene datos inválidos";
+                        } else{
+                            try{
+                                $idsNoExistentes = [];
+                                foreach($productosId as $id_producto){
+                                    if (!Producto::existeId($rol, $id_producto)){
+                                        $idsNoExistentes = " ".$id_producto;
+                                    }
+                                    if (!empty($idsNoExistentes)){
+                                        throw new Exception("Los ids: ".$idsNoExistentes." no se encuentran registrados");
+                                    }
+                                    // TODO: implementar
+                                }
+                            } catch(Exception $e){
+                                $response['errors'][] = $e->getMessage();
+                            }
+                            
+                        }
+                    } else{
+                        $response['errors'][] = "Debe ingresar el ID del cliente.";
+                    }                    
 
                 }else{
                     $response['errors'][] = "La orden debe contener al menos un ID de producto o reserva.";
@@ -48,8 +73,19 @@ class ControladorOrden extends AbstractController{
         }
     }
 
-    private function validarListaIds($array){
-        return is_array($array);
+    private function obtenerFechaHoraActual(){
+        $uruguayTimezone = new DateTimeZone('America/Montevideo');
+        $dtActual = new DateTime('now', $uruguayTimezone);
+
+        // Formatear la fecha
+        return $dtActual->format('Y-m-d H:i:s');
+    }
+
+    private function validarListaIds($idsRecibidos){
+        // Verifica si los elementos del arreglo son numéricos enteros mayores a cero
+        $idsValidos = array_filter($idsRecibidos, fn($id) => ctype_digit($id) && (int)$id > 0);
+        // Verifica si los arrays validados contienen la misma cantidad de elementos originales
+        return count($idsRecibidos) == count($idsValidos);
     }
 
 }
